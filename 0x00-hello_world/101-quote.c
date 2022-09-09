785e540f4f9d4c109648685a92b37191e1bec5fe
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-09 09:49:17 -0930

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /*
  * main-Entry point
  *
- * desciption=and that piece of art is useful" -Dura Korpar, 2015-10-19
+ * desciption=and that piece of art is useful" -Dora Korpar, 2015-10-19
  *
- *return: 1
+ *return: Always 1
 */
 int main(void)
 {
-write(1,"and that piece of art is usefule\" -Dura Korpar, 2015-10-19\n",59);
+write(1, "and that piece of art is usefule\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
