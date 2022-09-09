1e0124a3a6de690d35b9b6b0e2afc3add4535933
MichaelKetsela <michaelketsela837@gmail.com>
2022-09-09 12:15:14 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+/*
+ * main-Entry point
+ *
+ * desciption=and that piece of art is useful" -Dura Korpar, 2015-10-19
+ *
+ *return: 1
+*/
+int main(void)
+{
+write(1,"and that piece of art is usefule\" -Dura Korpar, 2015-10-19\n",59);
+return (1);
+}
