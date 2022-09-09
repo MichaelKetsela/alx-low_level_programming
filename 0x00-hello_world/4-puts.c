51f9822ff99f70d6f6135dffd0bf1879687a7ee2
MichaelKetsela <michaelketsela837@gmail.com>
2022-09-09 11:51:35 -0700

    updare

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 /**
  * main-Entry point
  *
- * put: print
- *
  * Result:Always 0 (Success)
  */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-result (0);
+return (0);
 }
