619add484bbf3a1ca5ef0919e55e99368b337b76
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-09 09:14:13 -0930

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
- 
-printf("Size of a char:%lu byte(s)\n",sizeof(ch));
-printf("Size of an int:%lu byte(s)\n",sizeof(i));
-printf("Size of an long int:%lu byte(s)\n",sizeof(li));
-printf("Size of an long long int:%lu byte(s)\n",sizeof(lli));
-printf("Size of an float:%lu byte(s)\n",sizeof(f));
- 
- return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(ch));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
 }
