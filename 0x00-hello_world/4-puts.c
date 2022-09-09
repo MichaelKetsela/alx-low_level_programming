7c8d60dde9cee7dd61818b168e7a5477324e0d78
MichaelKetsela <michaelketsela837@gmail.com>
2022-09-09 10:49:16 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main-Entry point
+ *
+ * put: print
+ *
+ * Result:Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+result (0);
+}
