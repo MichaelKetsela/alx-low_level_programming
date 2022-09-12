4776460ae998722239533d8d1bd0c2e2252a5827
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-12 08:24:27 -0930

    Create 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio>
+/**
+*main-Entry
+*
+*Description:'print the last digit'
+*
+*Return:Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
