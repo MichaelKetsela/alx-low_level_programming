2171b30ccb37cb28282ee4391bae364c7c8372bb
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-12 09:20:25 -0930

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:'print all number of base 16 in lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int h
+    h;
+
+	for (h = 48; h <= 57; h++)
+	{
+		putchar(h);
+	}
+
+	for (h = 97; h <= 102; h++)
+	{
+		putchar(h);
+	}
+	putchar(10);
+	return (0);
+}
