c0887c88efdb4515ba88d473e26bdd2a086250b1
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-12 09:11:12 -0930

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Discription : 'print all single digit numbers of base 10 starting from 0'
+ * Return: always 0
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d <= 57; d++)
+	{
+		putchar(d);
+	}
+	putchar(10);
+	return (0);
+}
