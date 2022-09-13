d54b30e775189890dcda7cba4e08fd910a78735f
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-13 11:46:46 -0930

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0.
+ */
+int _abs(int r)
+{
+if (n < 0)
+{
+r*=-1;
+printf("%d\n", r);
+}
+else
+{
+printf("%d\n", r);
+}
+return (0);
+}
