7503d113d9a66a60973e88c941aea06ef1387201
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-13 09:02:23 -0930

    Create _putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+*unistd.h - standard symbolic constants and types
+* _putchar - writes the character c to stdout
+* at c: The character to print
+* If you want to print new line on each output use c = '\n'; write(1,&c,1); at the end of the function
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately
+*/
+
+int _putchar(char c)
+{
+  return(write(1,&c,1));
+}
