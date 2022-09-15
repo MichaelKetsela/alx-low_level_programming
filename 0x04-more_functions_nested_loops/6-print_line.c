0e4ce8b7bc25aa64aa4582b434888545a1c0cade
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-15 12:41:39 -0930

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
 * print_line - draws a straight line in the terminal
-* n is the number of times the character _ should be printed
+* @n: is the number of times the character _ should be printed
 * The line should end with a \n
 * If n is 0 or less, the function should only print \n
 *
