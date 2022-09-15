50d7e4ba8f74d2400d58a81775a8fa31a800cc20
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-15 11:39:34 -0930

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * followed by a new line
+ * @i: int for the vertical
+ * @j: int for the horizontal
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0 ; i <= 10 ; i++)
+{
+int j;
+for (j = 0 ; j <= 14 : j++)
+{
+printf("%d ", j);
+}
+printf("\n");
+}
+}
