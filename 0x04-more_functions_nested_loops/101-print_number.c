09158abce08e842c8fff3d872500a53376f5545b
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-16 11:10:57 -0930

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* main - prints an integer
+* You are not allowed to use long
+* You are not allowed to use arrays or pointers
+* You are not allowed to hard-code special values
+* @n: the int
+* Return: Always 0.
+*/
+
+Prototype: void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar(num % 10 + '0');
+}
