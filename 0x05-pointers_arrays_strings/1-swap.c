7a554d5afec89ac81ad5faa919dffd7537dc0245
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-19 11:33:13 -0930

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,13 @@
 
 /**
 * swap_int -  swaps the values of two integers
-*
+* @a:the frist int
+* @b:the second int
 * Return: Always 0.
 */
 void swap_int(int *a, int *b)
 {
 int c;
-*a = 98;
-*b = 42;
-printf("a=%d, b=%d\n", *a, *b);
 c = *a;
 *a = *b;
 *b = c;
