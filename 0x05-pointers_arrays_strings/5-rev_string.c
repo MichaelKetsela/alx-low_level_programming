7b39fc3366d7afd4d45225c5d25e4fad293b1d04
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-19 13:29:37 -0930

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@ void rev_string(char *s)
 int l = strlen(s);
 /*strlen calculates the length of a given string*/
 int i = l - 1;
-char newstr [];
+char newstr[];
 while (i >= 0)
 {
-newstr [] = s[i];
+newstr[] = s[i];
 i--;
 }
 }
