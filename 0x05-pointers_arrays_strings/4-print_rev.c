25f428c9a08dd7091b0d0697e29ed049b09506b9
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-19 12:39:55 -0930

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line
+* @s: is the string
+* Return: Always 0.
+*/
+void print_rev(char *s)
+{
+int l = strlen(s);
+/*strlen calculates the length of a given string*/
+int i = l - 1;
+while (i >= 0)
+{
+printf("%s ", s[i]))
+i--;
+}
+}
