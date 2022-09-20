ad6755aadaa86bb032bb9b87e3f5d642c7317ca8
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-19 14:30:02 -0930

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
 * *_strcpy - Write a function that copies the string pointed to by src
 * including the terminating null byte (\0)
 * to the buffer pointed to by dest
-* @str: an string
+* @stc: an string
 * @dest: the string that str will be copied on
 * Return: Always 0.
 */
