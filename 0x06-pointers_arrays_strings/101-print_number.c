e33ae8ca18b8eb1e458bea06da471a1d5254833d
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 13:07:42 -0930

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,10 +2,12 @@
 
 /**
  * print_number - prints an integer
- *
- * Return: Always 0.
+ * @n: in integer
+ * Return: numbers
  */
 void print_number(int n)
 {
+if (n / 10)
+print_number(n / 10);
 _putchar((n % 10) + '0');
 }
