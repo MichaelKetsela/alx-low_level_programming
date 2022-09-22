f256221e42fbe3d39652a922d190cea38bfd2a4b
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 12:32:04 -0930

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,32 +4,30 @@
 * *cap_string - capitalizes all words of a string
 * @c: string
 * Separators of words: space, tabulation, new line, , , 
-* ;, ., !, ?, ", (, ), {, and }
+* ;, ., !, ?, ", (, ), {, and }  
 * Return: captal word
 */
 char *cap_string(char *z)
 {
-int i;
-int j;
-char s[] = " \n\t,;.!?\"(){}";
+int i = 0;
+	int j;
+	char badBoys[] = " \n\t,;.!?\"(){}";
 
-if (z[0] >= 'a' && z[0] <= 'z')
-z[0] -= 32;
 
-for (i = 0; z[i] != '\0'; i++)
-{
-for (j = 0; j < 14; j++)
-{
-if (z[i] != s[j])
-{
-if (z[i + 1] >= 'a' && z[i + 1] <= 'z')
-z[i + 1] -= 32;
-}
-else 
-{
-z[i] = s[j];
-}
-}
-}
-return (z);
+	if (z[0] >= 'a' && z[0] <= 'z')
+		z[0] -= 32;
+
+	for (; z[i] != '\0'; i++)
+	{
+		for (j = 0; j < 14; j++)
+		{
+			if (z[i] == badBoys[j])
+			{
+				if (z[i + 1] >= 'a' && z[i + 1] <= 'z')
+					z[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (z);
 }
