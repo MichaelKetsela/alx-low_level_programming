696d19a195f8dc04ce16a1a42cc718b34d2ed635
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 12:20:28 -0930

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,12 @@
 */
 char *cap_string(char *c)
 {
-	int i = 0;
-	int j;
-	char badBoys[] = " \n\t,;.!?\"(){}";
-
-
-	if (c[0] >= 'a' && c[0] <= 'z')
-		c[0] -= 32;
-
-	for (; c[i] != '\0'; i++)
-	{
-		for (j = 0; j < 14; j++)
-		{
-			if (c[i] == badBoys[j])
-			{
-				if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
-					c[i + 1] -= 32;
-			}
-		}
-	}
-
-	return (c);
+int j = strlen(c) - 1;
+int i;
+for (i = 0; i <= j; i++)
+{
+if (c[i] >= 97 && c[i] <= 122)
+c[i] -= 32;
+}
+return (c);
 }
