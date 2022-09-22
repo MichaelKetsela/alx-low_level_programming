7857788a7617a1d8b2c734afe0abe87d283e9412
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 09:58:51 -0930

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,10 +2,7 @@
 
 /**
 * reverse_array - reverses the content of an array of integers
-* @a: an array of integers
-* @n: the number of elements to swap
-*
-* Return: nothing.
+* Return: nothing
 */
 void reverse_array(int *a, int n)
 {
