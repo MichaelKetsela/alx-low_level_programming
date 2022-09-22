d7a10aa03e4329807681b86fdd89b0ebe71afbe7
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 12:49:53 -0930

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,27 @@
 #include "main.h"
 /**
- * rot13 - encodes a string using rot13
- *
- * @c: the string to encode
- *
- * Return: the new string
- */
+* rot13 - encodes a string using rot13
+*
+* @c: the string to encode
+*
+* Return: the new string
+*/
 
 char *rot13(char *c)
 {
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		for (j = 0; j < 56; j++)
-		{
-			if (z[i] == a[j])
-			{
-				c[i] = b[j];
-				break;
-			}
-		}
-	}
-
-	return (c);
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; c[i] != '\0'; i++)
+{
+for (j = 0; j < 56; j++)
+{
+if (c[i] == a[j])
+{
+c[i] = b[j];
+break;
+}
+}
+}
+return (c);
 }
