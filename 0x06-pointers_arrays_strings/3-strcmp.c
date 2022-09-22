f033b1f6bff600d29d32fa1878a42e23623f6932
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 09:11:59 -0930

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: the frist string
+* @s2: the second string
+* Return: Always 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
