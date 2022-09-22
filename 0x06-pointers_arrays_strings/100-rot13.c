10bab8616130f80a2afc6dcff0e04591d952a9ae
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-22 12:46:48 -0930

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @c: the string to encode
+ *
+ * Return: the new string
+ */
+
+char *rot13(char *c)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 56; j++)
+		{
+			if (z[i] == a[j])
+			{
+				c[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (c);
+}
