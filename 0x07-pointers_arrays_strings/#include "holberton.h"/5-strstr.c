e2ee5ef943848516aeeb15d9721cda38b6d8497d
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-26 08:50:42 -0930

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/#include "holberton.h"/5-strstr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _strstr - locates a substring
+*
+* @haystack: the longer string to search
+* @needle: the substring to search for
+*
+* Return: a pointer to the beginning of the located substring, or NULL if
+* the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char *r = strstr(haystack, needle);
+return (r);
+}
