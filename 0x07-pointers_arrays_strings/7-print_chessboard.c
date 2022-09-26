0fe0ef761c30b4e07178d25d4f4eef135bda8530
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-26 09:07:57 -0930

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+ {
+ int i, j;
+ for (i = 0; i <= 7 ; i++)
+ {
+ for (j = 0; j <= 7 ; j++)
+ {
+ putchat((*a)[j]);
+ }
+ putchat('\n');
+ }
+ }
