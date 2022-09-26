db44f4a5eab6114357da2a897feb27933fd3409c
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-26 09:14:08 -0930

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,14 @@
  * Return: nothing
  */
 void print_chessboard(char (*a)[8])
- {
- int i, j;
- for (i = 0; i <= 7 ; i++)
- {
- for (j = 0; j <= 7 ; j++)
- {
- putchar(a[i][j]);
- }
- putchar('\n');
- }
- }
+{
+int i, j;
+for (i = 0; i <= 7 ; i++)
+{
+for (j = 0; j <= 7 ; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
+}
