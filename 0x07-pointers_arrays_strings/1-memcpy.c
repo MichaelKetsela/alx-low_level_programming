17b8aa41a4694fb1e6d6d102cf7f1295c8dccad8
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-26 07:53:24 -0930

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int j = i;
-memcpy(dest, src, j );
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
 Return ( dest );
 }
