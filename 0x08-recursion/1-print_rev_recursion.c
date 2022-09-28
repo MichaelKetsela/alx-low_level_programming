a567e9c3ae1e74b591fd41c14eb2a9d5ed765f7b
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 09:18:08 -0930

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: the string to reverse
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+int j = lenstr(s);
+int i = 0;
+char rev;
+while (i < j)
+{
+
+}
+}
