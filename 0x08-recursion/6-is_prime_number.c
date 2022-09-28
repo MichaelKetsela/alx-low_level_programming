830df2ec97a23bdbd755b9b7e630ec9485fb6321
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 12:51:13 -0930

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number - tells us if an integer is a prime number or not
+ *
+ * @n: the number to check
+ *
+ * Return: 0 if the number is not prime, and 1 if it is
+ */
+
+int is_prime_number(int n)
+{
+if (n % 2 == 0)
+return (1);
+  
+return (0);
+}
