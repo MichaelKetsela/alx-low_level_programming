f95860d30eb721b4d12090e3c372349ce483f2a2
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 11:59:58 -0930

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,13 +8,13 @@
  *
  * Return: the value of @x to the @y power
  */
- int _pow_recursion(int x, int y)
- {
- if (y < 0)
- return(-1);
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return(-1);
+
+if (y == 0)
+return (1);
  
- if (y == 0)
- return (1);
- 
- return(x * (_pow_recursion(x , --y)));
- }
+return(x * (_pow_recursion(x , --y)));
+}
