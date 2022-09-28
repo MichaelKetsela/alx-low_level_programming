0420e1dd73a0bfd6b6c24c10a2c58a39877ec68f
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 12:34:11 -0930

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,17 +4,21 @@
  * helper - Finds the square root of two numbers
  *
  * @n: The number
- *
+ * @i: The squared number
+ * @j: The square root
  * Return: square root
  */
-int square(int i , int j)
+int square(int i, int j)
 {
 if (j * j > i)
 return (-1);
-  
+
 else if (j * j == i)
 return (j);
-   
+
+else
+return (square(i, ++j))
+
 return (1);
 }
 int _sqrt_recursion(int n)
@@ -22,5 +26,5 @@ int _sqrt_recursion(int n)
 if (n == 0)
 return (0);
 
-return (square(n , 1));
+return (square(n, 1));
 }
