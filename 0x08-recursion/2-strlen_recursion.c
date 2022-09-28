c54dba4a34302f19c100640f003ec4ca4c431a66
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 10:33:16 -0930

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
 * _strlen_recursion - returns the length of a string
 *
-* @s: the string 
+* @s: the string
 *
 * Return: an integer that displays the length of the string
 */
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 {
 if (*s != '\0')
 {
-return (1 + _strlen_recursion(s + 1) );
+return (1 + _strlen_recursion(s + 1));
 }
 return (0);
 }
