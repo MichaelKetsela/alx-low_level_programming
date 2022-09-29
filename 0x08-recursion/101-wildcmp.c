6f32861080683e5e38681526e27be7ea6bec6bf9
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 15:28:26 -0930

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
+/**
+ * wildcmp - compares two strings
+ * @s1: the frist string
+ * @s2: the second string
+ * Return: 0 or 1.
+ */
 int wildcmp(char *s1, char *s2)
 {
-if (!strcmp(s1 , s2))
+if (!strcmp(s1, s2))
 return (1);
 else
 return (0);
