aa2293d2049dc68acb4f914c195480a8a29a3f95
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-28 15:13:40 -0930

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,9 @@
 #include "main.h"
 
-int cmp(char *a, char *b )
-{
-int l1 = strlen(a);
-int l2 = strlen(b);
-int i = 0;
-int j = 0;
-if (l1 != l2)
-return 0;
-
-else if (i < l1)
-{
-if (a[i] == b[i])
-i++;
-j++;
-cmp(a, b);
-}
-if (i == j)
-return (1);
-
-return (0);
-}
-
 int wildcmp(char *s1, char *s2)
 {
-return (cmp(s1, s2));
+if (!strcmp(s1 , s2))
+return (1);
+else
+return (0);
 }
