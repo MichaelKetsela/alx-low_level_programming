af4ff3e474df988d2dfc1b1bb12ce70aa7f17155
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-09-30 08:05:35 -0930

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,13 @@
 
 int main(int argc, char *argv[])
 {
+int m;
 if(argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int m = atoi(argv[1]) * atoi(argv[2]);
+m = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", m);
 return (0);
 }
