9114c37958b1754ce25d24551543246ad6fca70e
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 13:25:00 -0930

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ *
+ * @width: Width of grid
+ * @height: height of grid
+ *
+ * Return: NULL on failure, NULL if @width or @height is 0 or negative,
+ * on success: a pointer to a 2 dimensional array of integers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i;
+	int j;
+	int k;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (ar[i] == NULL)
+		{
+			for (i >= 0; i = 0; i--)
+			{
+				free(a[i]);
+			}
+			free(a);
+			return (NULL);
+		}
+	}
+
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+			ar[j][k] = 0;
+	}
+	return (a);
+}
