2596ef39b0aa88b8fb026ba3cb3ea020c1692a50
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 14:24:03 -0930

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,43 +9,23 @@
  */
 char **strtow(char *str)
 {
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, c = 0, start, end, w = 0;
-
-	while (*(str + len))
-		len++;
+	char *sp;
+	int w = 0;
+	
+	if (str == NULL)
+		return (NULL);
+		
 	while (srt[w])
 		w++;
-	if (w == 0)
-		return (NULL);
-
-	matrix = (char **) malloc(sizeof(char *) * (w + 1));
-	if (matrix == NULL)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
+	
+	sp = malloc(sizeof(char) * (w + 1));
+	sp = strtok(str, "\t");
+	while (sp != NULL)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
+		printf("%s\n", sp);
+		sp = strtok(NULL, "\t");
 	}
+	
 
-	matrix[k] = NULL;
-
-	return (matrix);
+	return (a);
 }
