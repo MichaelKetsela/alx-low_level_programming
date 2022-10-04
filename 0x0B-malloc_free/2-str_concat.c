24b37185b33ed4345b2ae05db7740d7c4e7fb0c7
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 12:21:16 -0930

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int l1 = 0;
 	unsigned int l2 = 0;
 	unsigned int l3;
-	unsigned int i = 0;
-	unsigned int j = 0;
-
+	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -33,7 +31,7 @@ char *str_concat(char *s1, char *s2)
 	l3 = l1 + l2;
 
 	con = malloc(sizeof(char) * l3 + 1);
-	if (s3 == NULL)
+	if (con == NULL)
 		return (NULL);
   
   con = strcat (s1, s2);
