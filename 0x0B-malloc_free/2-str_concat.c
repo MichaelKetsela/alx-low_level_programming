88a410b984704a56c52cd98d22217b64acd40be5
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 13:11:29 -0930

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int s1len = 0;
-	unsigned int s2len = 0;
-	unsigned int s3len;
+	char *concat;
+	unsigned int l1 = 0;
+	unsigned int l2 = 0;
+	unsigned int l3;
 	unsigned int i = 0;
 	unsigned int j = 0;
 
@@ -25,28 +25,28 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[s1len])
-		s1len++;
-	while (s2[s2len])
-		s2len++;
+	while (s1[l1])
+		l1++;
+	while (s2[l2])
+		l2++;
 
-	s3len = s1len + s2len;
+	l3 = l1 + l2;
 
-	s3 = malloc(sizeof(char) * s3len + 1);
-	if (s3 == NULL)
+	concat = malloc(sizeof(char) * l3 + 1);
+	if (concat == NULL)
 		return (NULL);
 
-	while (i < s1len)
+	while (i < l1)
 	{
-		s3[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 
-	while (i <= s3len)
+	while (i <= concat)
 	{
-		s3[i] = s2[j];
+		concat[i] = s2[j];
 		i++;
 		j++;
 	}
-	return (s3);
+	return (concat);
 }    
