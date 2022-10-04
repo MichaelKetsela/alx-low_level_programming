d1c2c18015185e9deb53584355dd11e8d9cc49ab
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 10:46:22 -0930

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-unsigned i;
+unsigned int i;
 char *s;
 s = malloc(1 * size);
-if (size == 0)
+if (size == 0 || s == NULL)
 return (NULL);
 
 i = 0;
