6d8d92019b9e1c7c741a88df6ab43ebdb1475011
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 15:34:20 -0930

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -8,5 +8,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-return (strcmp(s1, s2));
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
 }
