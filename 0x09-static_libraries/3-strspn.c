918370a089ebefa4058be8a61f0aae5e3ebb1543
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 15:35:33 -0930

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -11,6 +11,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int u = strspn(s, accept);
-return (u);
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
+	}
+	return (c);
 }
