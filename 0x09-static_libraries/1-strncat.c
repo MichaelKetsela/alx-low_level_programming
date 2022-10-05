90827a319c92125d75fe6f02c601c6f915f32be5
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 15:13:19 -0930

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -9,5 +9,20 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-return (strncat(dest, src, n));
+	int len1 = 0; /* string length for src */
+	int len2 = 0; /* string length for dest */
+	int i;
+
+	while (src[len1] != '\0')
+		len1++;
+
+	while (dest[len2] != '\0')
+		len2++;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len2] = src[i];
+		len2++;
+	}
+	return (dest);
 }
