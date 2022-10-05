64568339262dbcdfc0a2cec358833f6a67683f12
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-04 15:07:26 -0930

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -8,8 +8,14 @@
 *
 * Return: a pointer to @dest
 */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-memcpy(dest, src, n);
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
 }
