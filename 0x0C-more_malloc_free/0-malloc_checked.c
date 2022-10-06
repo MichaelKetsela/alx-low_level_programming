5e7569758f28ff9ec019ef3c806955d5a86b1fa9
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-06 10:41:49 -0930

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Entry Point
+ *
+ * @b: the number to allocate memory for
+ *
+ * Return: pointer to the new allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    unsigned *n;
+    n = malloc(b);
+    if (n == NULL)
+          exit(98);
+    return (n);
+}
