7ca1cf1e8051bfbfacb747678ff9b69b768c56f2
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-06 11:26:21 -0930

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-    unsigned int *n;
-    
-    n = malloc(b);
-    if (n == NULL)
-        exit(98);
-    return (n);
+unsigned int *n;
+
+n = malloc(b);
+if (n == NULL)
+exit(98);
+return (n);
 }
