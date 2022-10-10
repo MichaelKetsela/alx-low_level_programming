930149e2041598f699cd36abba8080163f71f92f
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-10 11:59:12 -0930

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,18 @@
 #ifndef DOG
 #define DOG
+
 /**
  * struct dog - a strcture
  * @name: a string
  * @age: an int
  * @owner: a string
  */
+ 
 struct dog
 {
 char *name;
 float *age;
 char *owner;
 };
+
 #endif
