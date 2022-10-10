33bc91283ee50fbcf50f075457796fc7caefb4e0
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-10 14:24:01 -0930

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print name of file compiled from, followed by
+ * a new line
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
