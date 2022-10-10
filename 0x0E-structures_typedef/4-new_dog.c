599a9a85ac9620d5bea35d95fd6641ec2c37bd5d
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-10 12:55:13 -0930

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+
+/**
+ * new_dog - new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	int i = 0, j = 0, k;
+	dog_t *newdog;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+	{
+		free(doge);
+		return (NULL);
+	}
+	newdog->name = malloc(i * sizeof(newdog->name));
+	if (newdog->name == NULL)
+	{
+		free(newdog->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		newdog->name[k] = name[k];
+	newdog->age = age;
+	newdog->owner = malloc(j * sizeof(newdog->owner));
+	if (newdog->owner == NULL)
+	{
+		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		newdog->owner[k] = owner[k];
+	return (newdog);
+}
