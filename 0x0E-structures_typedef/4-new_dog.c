3aae39f0cbe2f6208087a47e70df64f6251a46d0
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-10 13:08:30 -0930

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,19 +18,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 		i++;
 	while (owner[j] != '\0')
 		j++;
+	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 	{
+		free(newdog);
 		return (NULL);
 	}
+	newdog->name = malloc(i * sizeof(newdog->name));
 	if (newdog->name == NULL)
 	{
+		free(newdog->name);
+		free(newdog);
 		return (NULL);
 	}
 	for (k = 0; k <= i; k++)
 	newdog->name[k] = name[k];
 	newdog->age = age;
+	newdog->owner = malloc(j * sizeof(newdog->owner));
 	if (newdog->owner == NULL)
 	{
+		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
 		return (NULL);
 	}
 	for (k = 0; k <= j; k++)
