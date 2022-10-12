978d64c4dd94f59c5d0ced7e8c6b2c7e18751843
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 11:22:29 -0930

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: a string 
+ * @f: the function to send the name to
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
