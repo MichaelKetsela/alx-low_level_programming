eebe0dc974e4d07df08f0336721d8a15ddd4cfa4
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 12:22:35 -0930

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
 
-if (size <= 0 || !array || !cmp)
+if (array == NULL || size <= 0 || cmp == NULL)
   return (-1);
 
 if (array && size && cmp)
