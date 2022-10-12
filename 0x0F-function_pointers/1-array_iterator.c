ef019636371f09f58a6fe14bcb2e1d3c44661b59
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 11:55:18 -0930

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int i = 0;
+if (array && size &&action)
 while (i < size)
 {
-if (array && action)
 action(array[i]);
-
 i++;
 }
 
