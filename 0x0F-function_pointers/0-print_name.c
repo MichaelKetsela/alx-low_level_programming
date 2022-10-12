9605b9cfb6de77103a411bf0f0af6a7c6ce56319
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 11:39:09 -0930

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,15 @@
 
 /**
  * print_name - prints the name
- * @name: a string 
+ * @name: a string
  * @f: the function to send the name to
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+
+if (name && f)
 f(name);
+
 }
