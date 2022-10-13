3f5cfaf471ff75c21040b706f33b1bc9d2a304eb
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 14:48:46 -0930

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,10 @@
 #ifndef CALC
 #define CALC
-
+#include <stdio.h>
+#include <stdlib.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 #endif
