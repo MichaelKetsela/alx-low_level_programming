2c262c597a18c6bbee36db873126091016dee878
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 14:46:05 -0930

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,21 +1,51 @@
 #include "3-calc.h"
 
+/**
+ * op_add - add
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
 int op_add(int a, int b)
 {
 return (a + b);
 }
+/**
+ * op_sub - subtract
+ * @a: first int
+ * @b: second int
+ * Return: difference
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+/**
+ * op_mul - multiplies
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+/**
+ * op_div - divides
+ * @a: first int
+ * @b: second int
+ * Return: result
+ */
 int op_div(int a, int b)
 {
 return (a / b);
 }
+/**
+ * op_mod - remainder
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
 int op_mod(int a, int b)
 {
 return (a % b);
