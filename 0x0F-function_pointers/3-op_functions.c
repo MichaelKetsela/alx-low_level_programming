a1538cc56c7a3aa652980b3cfed2b7c02dc01773
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-12 15:22:32 -0930

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -38,6 +38,11 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 return (a / b);
 }
 /**
@@ -48,5 +53,10 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
+if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 return (a % b);
 }
