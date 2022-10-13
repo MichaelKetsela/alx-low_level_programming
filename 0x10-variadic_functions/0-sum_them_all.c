5762ca971fef12f1d5bcc391776b1456d735258d
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-13 10:57:14 -0930

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns the sum of all parameters
+ * @n: the numbers of arguments being passed through
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i, sum = 0;
+if (n == 0)
+  return(0);
+va_start(ap, n);
+for(i = n; i >= 0; i = va_arg(ap, n))
+{ 
+  sum += va_arg(ap, n);
+}
+va_end(ap);
+return (j);
+}
