de58ada6767cf2aa4f5c4d134721ffd063cc2d1e
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-25 11:52:16 -0930

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (y = 0; y < index; y++)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 	return (head);
 }
