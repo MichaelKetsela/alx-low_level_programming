486212d45699e0678d0a2c0b8058fd2479678083
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-27 11:21:00 -0930

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: points to a string of 0 and 1 characters
+ * Return: the converted number or 0 if there is one or more characters in
+ * string @b that is not 0 or 1, or @b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int k = 2, i = 0, c, len;
+
+    if (b == NULL)
+        return (0);
+    len = strlen(b);
+    for (c = len; c > 0; c--)
+    {
+        if (b[c] != 48 || b[c] != 49)
+            return (0);
+        if (b[c] == '1')
+        {
+            i += atoi(b) * k;
+        }
+        k *= 2;
+    }
+    return (i);
+}
