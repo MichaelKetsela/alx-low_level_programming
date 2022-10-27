ba180c41643e68c65f9a3fcde85df9b7eb9a1550
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-27 14:13:42 -0930

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
     if (b == NULL)
         return (0);
     len = strlen(b);
-    for (c = len; c > 0; c--)
+    for (c = len; c >= 0; c--)
     {
         if (b[c] != 48 || b[c] != 49)
             return (0);
