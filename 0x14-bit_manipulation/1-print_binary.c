1b377223ea1025a4e6bb926c1906ea7a315e086b
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-27 14:36:04 -0930

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,8 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-if(n > 1)
-print_binary(n / 2);
-  
-printf("%d", n % 2);
+unsigned long int i;
+for (i = 1 << 63; i > 0; i = i / 2)
+(n & i) ? printf("1") : printf("0");
 }
