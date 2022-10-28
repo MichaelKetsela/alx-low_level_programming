262d08c830aaeda711398c84dd452b77044e3a38
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-27 14:46:57 -0930

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,12 +10,9 @@
  
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int *num;
+	if (index > 63)
+		return (-1);
 
-if (index > 63)
-  return (-1);
-
-num = ((1UL << index) | *n);
-printf("%d", num);
-return(1);
+	*n = ((1UL << index) | *n);
+	return (1);
 }
