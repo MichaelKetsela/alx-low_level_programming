62ee7a3879272f4e08e0c56cbae1dd7dc098c05c
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-08 12:52:56 -0930

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - print all the element
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a node at a given index
  * @h: double pointer to the list
  * @idx: index of the node to insert
