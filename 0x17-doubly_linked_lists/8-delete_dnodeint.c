30630c7072e74c565e12739ac49a1233288350f4
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-12-08 13:00:18 -0930

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,26 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - print all the element
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
+}
+
 /**
  * dlistint_len - returns the number of elements in a linked list
  * @h: pointer to head of a list
